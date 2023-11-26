1f730eb29ea461dbfd0ad389f5b9ddde8fc2e1d6
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-26 18:29:18 +0900

    Add sample saved kv item

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { XMLParser } from "fast-xml-parser";
 export type Env = {
 	URL: string;
+	BOOK_LAUNCH: KVNamespace;
 };
 
 type XMLContemt = {
@@ -13,6 +14,7 @@ type XMLItem = {
 	title: string;
 	date: string;
 	link: string;
+	expiration: number
 };
 
 
@@ -28,16 +30,34 @@ export default {
 			removeNSPrefix: true,
 		});
 		const json: XMLContemt = parser.parse(text);
-
-		const items: XMLItem[] = [];
-		for (const i of json.RDF.item) {
-			items.push({
+		const items: XMLItem[] = json.RDF.item.map(i => {
+			const date = new Date(i.date);
+			date.setDate(date.getDate() + 1);
+			const expiration = Math.floor(date.getTime() / 1000);
+			return {
 				title: i.title,
 				date: i.date,
 				link: i.link,
-			});
-		}
+				expiration
+			};
+		});
 
-		return new Response(JSON.stringify(items));
+		const keys = (await env.BOOK_LAUNCH.list()).keys.reduce((acc, i) => {
+			acc[i.name] = true;
+			return acc;
+		}, {} as Record<string, boolean>);
+
+		const unsavedItems: XMLItem[] = [];
+
+		for (const item of items) {
+			const url = new URL(item.link);
+			const key = url.pathname;
+			if (keys[key]) {
+				continue;
+			}
+			await env.BOOK_LAUNCH.put(key, JSON.stringify(item), {});
+			unsavedItems.push(item);
+		}
+		return new Response(JSON.stringify(unsavedItems));
 	},
 };
