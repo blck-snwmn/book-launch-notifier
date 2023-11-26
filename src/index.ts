be73960cfda561250e856aae61c45216b63272bf
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-26 17:29:36 +0900

    Parse XML

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,43 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { XMLParser } from "fast-xml-parser";
+export type Env = {
+	URL: string;
+};
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+type XMLContemt = {
+	RDF: {
+		item: XMLItem[];
+	};
+};
+
+type XMLItem = {
+	title: string;
+	date: string;
+	link: string;
+};
+
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const resp = await fetch(env.URL);
+		if (!resp.ok) {
+			return new Response("Error");
+		}
+		const text = await resp.text();
+
+		const parser = new XMLParser({
+			removeNSPrefix: true,
+		});
+		const json: XMLContemt = parser.parse(text);
+
+		const items: XMLItem[] = [];
+		for (const i of json.RDF.item) {
+			items.push({
+				title: i.title,
+				date: i.date,
+				link: i.link,
+			});
+		}
+
+		return new Response(JSON.stringify(items));
 	},
 };
