a8fccbfa2bed73808b14f26f1d1c8e0101ef6917
blck-snwmn <whitesnowmancreator@gmail.com>
2023-11-26 18:41:12 +0900

    Use hono

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { XMLParser } from "fast-xml-parser";
-export type Env = {
+import { Hono } from "hono";
+
+export type Bindings = {
 	URL: string;
 	BOOK_LAUNCH: KVNamespace;
 };
@@ -17,47 +19,58 @@ type XMLItem = {
 	expiration: number
 };
 
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const resp = await fetch(env.URL);
-		if (!resp.ok) {
-			return new Response("Error");
+app.post("/items", async (c) => {
+	const resp = await fetch(c.env.URL);
+	if (!resp.ok) {
+		return new Response("Error");
+	}
+	const text = await resp.text();
+
+	const parser = new XMLParser({
+		removeNSPrefix: true,
+	});
+	const json: XMLContemt = parser.parse(text);
+	const items: XMLItem[] = json.RDF.item.map(i => {
+		const date = new Date(i.date);
+		date.setDate(date.getDate() + 1);
+		const expiration = Math.floor(date.getTime() / 1000);
+		return {
+			title: i.title,
+			date: i.date,
+			link: i.link,
+			expiration
+		};
+	});
+
+	const keys = (await c.env.BOOK_LAUNCH.list()).keys.reduce((acc, i) => {
+		acc[i.name] = true;
+		return acc;
+	}, {} as Record<string, boolean>);
+
+	const unsavedItems: XMLItem[] = [];
+
+	for (const item of items) {
+		const url = new URL(item.link);
+		const key = url.pathname;
+		if (keys[key]) {
+			// ignore if already saved
+			continue;
 		}
-		const text = await resp.text();
+		await c.env.BOOK_LAUNCH.put(key, JSON.stringify(item), {});
+		unsavedItems.push(item);
+	}
+	return new Response(JSON.stringify(unsavedItems));
+})
 
-		const parser = new XMLParser({
-			removeNSPrefix: true,
-		});
-		const json: XMLContemt = parser.parse(text);
-		const items: XMLItem[] = json.RDF.item.map(i => {
-			const date = new Date(i.date);
-			date.setDate(date.getDate() + 1);
-			const expiration = Math.floor(date.getTime() / 1000);
-			return {
-				title: i.title,
-				date: i.date,
-				link: i.link,
-				expiration
-			};
-		});
-
-		const keys = (await env.BOOK_LAUNCH.list()).keys.reduce((acc, i) => {
-			acc[i.name] = true;
-			return acc;
-		}, {} as Record<string, boolean>);
-
-		const unsavedItems: XMLItem[] = [];
-
-		for (const item of items) {
-			const url = new URL(item.link);
-			const key = url.pathname;
-			if (keys[key]) {
-				continue;
-			}
-			await env.BOOK_LAUNCH.put(key, JSON.stringify(item), {});
-			unsavedItems.push(item);
-		}
-		return new Response(JSON.stringify(unsavedItems));
+const scheduled = {
+	async scheduled(controller: ScheduledController, env: Bindings): Promise<void> {
 	},
 };
+
+
+export default {
+	...app,
+	...scheduled,
+}
